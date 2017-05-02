af14b334c684b7ce958c7cd8016f9cac9e8cc47b
danielbicho <daniel.bicho@fccn.pt>
2017-05-02 15:31:42 +0100

    change setup.py to install correctly submodules

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='SafeImage',
     version='0.1',
-    packages=['images_classifiers'],
+    packages=find_packages(),
+    package_data={'':['*.npy']},
+    include_package_data=True,
     url='https://github.com/danielbicho/SafeImage',
     license='',
     author='dbicho',
     author_email='daniel.bicho@fccn.pt',
     description='REST Web Service to classify an image if it has adult free content or not',
-    install_requires=['tensorflow', 'Pillow', 'Flask', 'Flask_Autodoc', 'Flask_RESTful' ]
+    #install_requires=['tensorflow', 'Pillow', 'Flask', 'Flask_Autodoc', 'Flask_RESTful', 'numpy']
 )
