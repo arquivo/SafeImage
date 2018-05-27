c300b1168c531bbcef66fae292aafe08a37699ae
Daniel Bicho <daniel.bicho@fccn.pt>
2018-05-27 13:35:17 +0100

    Add NsfwSqueezeNet Model

--- tests/test_cf_nsfwsqueeze.py
+++ tests/test_cf_nsfwsqueeze.py
@@ -1,22 +1,22 @@
-from images_classifiers.cf.classifiers import CaffeNsfwResnetClassifier
+from images_classifiers.cf.classifiers import CaffeNsfwSqueezeClassifier
 
 
 def test_classify():
-    model = CaffeNsfwResnetClassifier(batch_size=1)
+    model = CaffeNsfwSqueezeClassifier(batch_size=1)
 
     with open('image_test_1.jpg', mode='rb') as input_file:
         result = model.classify(input_file.read())
 
-    assert result[0] == 0.008
+    assert result[0] == 0.0
 
 
 def test_batch_classification():
-    model = CaffeNsfwResnetClassifier(batch_size=2)
+    model = CaffeNsfwSqueezeClassifier(batch_size=2)
 
     with open('image_test_1.jpg', mode='rb') as input_file:
         image_bytes = input_file.read()
         image_data = [image_bytes, image_bytes]
         result = model.classify_batch(image_data)
 
-        assert result[0] == 0.008
-        assert result[1] == 0.008
+        assert result[0] == 0.0
+        assert result[1] == 0.0
