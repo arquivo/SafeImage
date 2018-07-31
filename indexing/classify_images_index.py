a9e2739158af24fa22669aee136eab99325c495d
Daniel Bicho <daniel.bicho@fccn.pt>
2018-07-31 15:01:01 +0100

    Index Jsonl

--- indexing/classify_images_index.py
+++ indexing/classify_images_index.py
@@ -1,6 +1,5 @@
 import json
 import os
-import fnmatch
 import base64
 import time
 
@@ -13,30 +12,36 @@ batch_size = 1
 for filename in os.listdir('test_files/'):
     path = os.path.join('test_files/', filename)
 
-    if os.path.isfile(path) and fnmatch.fnmatch(filename, 'part-*'):
-        with open('indexed_test_files/{}'.format(filename), mode='a') as output:
-            with open(path) as f:
-                print('******* {}'.format(path))
+    with open('indexed_test_files/nsfw_{}'.format(filename), mode='a') as output_file:
+        with open(path) as input_file:
+            print('******* {}'.format(path))
+
+            for line in input_file:
                 batch_json_docs = []
                 batch_img = []
 
                 start = time.time()
-                for line in f:
-                    try:
-                        json_doc = json.loads(line)
-                    except Exception as e:
-                        print(e)
-                        print(line)
+                try:
+                    json_doc = json.loads(line)
+                except Exception as e:
+                    print(e)
+                    print(line)
 
-                    batch_json_docs.append(json_doc)
-                    batch_img.append(base64.b64decode(json_doc['imgSrcBase64']))
+                batch_json_docs.append(json_doc)
+                batch_img.append(base64.b64decode(json_doc['imgSrcBase64']))
 
-                    if len(batch_img) == batch_size:
-                        results = classifier.classify_batch(batch_img)
-                        for i in range(len(results)):
-                            json_doc = batch_json_docs[i]
-                            json_doc['nsfw'] = results[i]
-                            output.write(json.dumps(json_doc))
-                        end = time.time()
-                        print('Records / Second: {}'.format(end - start))
+                if len(batch_img) == batch_size:
+                    results = classifier.classify_batch(batch_img)
+                    for i in range(len(results)):
+                        json_doc = batch_json_docs[i]
+                        json_doc['safe'] = results[i]
+                        output_file.write(json.dumps(json_doc))
+                    end = time.time()
+                    print('Records / Second: {}'.format(batch_size/(end - start)))
 
+            if len(batch_img) != 0:
+                results = classifier.classify_batch(batch_img)
+                for i in range(len(results)):
+                    json_doc = batch_json_docs[i]
+                    json_doc['safe'] = results[i]
+                    output_file.write(json.dumps(json_doc))
