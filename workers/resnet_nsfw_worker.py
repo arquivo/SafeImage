ec474b290ae664156356c6db0d111b4bc858603f
Daniel Bicho <daniel.bicho@fccn.pt>
2018-07-31 14:19:55 +0100

    update resnet_worker

--- workers/resnet_nsfw_worker.py
+++ workers/resnet_nsfw_worker.py
@@ -13,47 +13,46 @@ def main():
     parser.add_argument('hostname', default='localhost', help='Specify Redis Server hostname.')
     parser.add_argument('port', default=6379, help='Specify Redis Server listening port.')
     parser.add_argument('batch_size', default=1, help='Specify the batch size to classify.')
-    parser.add_argument('fetch_size', default=1, help='Specify the fetch size to get from Redis.')
-    parser.add_argument('polling_time', default=0.25, help='Polling time interval in seconds.')
+    parser.add_argument('polling_time', default=0.1, help='Polling time interval in seconds.')
 
     args = parser.parse_args()
 
-    init_worker(args.hostname, args.port, int(args.batch_size), int(args.fetch_size), float(args.polling_time))
+    init_worker(args.hostname, args.port, int(args.batch_size), float(args.polling_time))
 
 
-def init_worker(hostname, port, batch_size, fetch_size, polling_time):
+def init_worker(hostname, port, batch_size, polling_time):
     db = redis.StrictRedis(host=hostname, port=port)
 
     classifier = CaffeNsfwResnetClassifier(batch_size=batch_size)
 
     while True:
         start = time.time()
-        fetch_queue = db.lrange("image_queueing", 0, fetch_size)
+        queue = db.lrange("image_queueing", 0, batch_size)
 
-        queues = [fetch_queue[:batch_size], fetch_queue[batch_size:]]
-        for queue in queues:
-            image_ids = []
-            batch = []
-            for q in queue:
-                q = json.loads(q.decode("utf-8"))
-                image = q["image"]
+        image_ids = []
+        batch = []
+        for q in queue:
+            q = json.loads(q.decode("utf-8"))
+            image = q["image"]
 
-                batch.append(base64.b64decode(image))
-                image_ids.append(q["id"])
+            batch.append(base64.b64decode(image))
+            image_ids.append(q["id"])
 
-            if len(image_ids) > 0:
-                print("* Batch size: {}".format(len(batch)))
-                results = classifier.classify_batch(batch)
+        if len(image_ids) > 0:
+            print("* Batch size: {}".format(len(batch)))
+            results = classifier.classify_batch(batch)
 
-            for i in range(len(image_ids)):
-                db.set(image_ids[i], json.dumps(results[i]))
+        for i in range(len(image_ids)):
+            db.set(image_ids[i], json.dumps(results[i]))
 
-            db.ltrim("image_queueing", len(image_ids), -1)
+        db.ltrim("image_queueing", len(image_ids), -1)
 
         end = time.time()
         elapsed_time = end - start
         time.sleep(polling_time)
-        print("* Images/Second: {}".format(fetch_size / (elapsed_time + polling_time)))
+
+        print("** Batch Size: {}".format(len(batch)))
+        print("** Images/Second: {}".format(len(batch) / (elapsed_time + polling_time)))
 
 
 if __name__ == '__main__':
